cf3ecd97b96757c281718cb800113841a884bf50
Ivan Wang <bobo8347@gmail.com>
2018-02-04 13:12:21 +0800

    - Using Set to handle path

--- src/main/java/config/CustomFilter.java
+++ src/main/java/config/CustomFilter.java
@@ -2,6 +2,10 @@ package config;
 
 import components.UserInfo;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -24,6 +28,8 @@ import javax.servlet.http.HttpSession;
 public class CustomFilter implements Filter {
 
   private String encoding = "utf-8";
+  private static final Set<String> EXCLUDED_PATHS = Collections.unmodifiableSet(new HashSet<>(
+      Arrays.asList("", "/", "/Login")));
 
   /**
    * Filter all url.
@@ -40,17 +46,15 @@ public class CustomFilter implements Filter {
     HttpServletRequest httpServletRequest = (HttpServletRequest) request;
     HttpSession session = httpServletRequest.getSession(true);
     boolean isLogin = UserInfo.fetchInfoFromSession(session) != null;
-    boolean isLoginUrl = httpServletRequest.getRequestURI()
-        .equals(httpServletRequest.getContextPath() + "/Login");
-    boolean isRootUrl = httpServletRequest.getRequestURI()
-        .equals(httpServletRequest.getContextPath() + "/");
     boolean isResourceFile = httpServletRequest.getRequestURI()
         .contains(httpServletRequest.getContextPath() + "/resources");
     boolean isResponseError = httpServletResponse.getStatus() != HttpServletResponse.SC_OK;
-
+    boolean isExcludedPath = EXCLUDED_PATHS.contains(
+        httpServletRequest.getRequestURI().substring(httpServletRequest.getContextPath().length())
+            .replaceAll("[/]+$", ""));
     // If logged in or at root page/login page or resource file or response status error, ignore it.
     // Else redirect to root page
-    if (isLogin || isLoginUrl || isRootUrl || isResourceFile || isResponseError) {
+    if (isLogin || isExcludedPath || isResourceFile || isResponseError) {
       if (httpServletResponse.getHeader("X-Frame-Options") == null) {
         httpServletResponse.addHeader("X-Frame-Options", "SAMEORIGIN");
       }
@@ -69,7 +73,6 @@ public class CustomFilter implements Filter {
    * Add encoding.
    *
    * @param filterConfig FilterConfig
-   * @throws ServletException exception
    */
   public void init(FilterConfig filterConfig) {
     String encodingParam = filterConfig.getInitParameter("encoding");
